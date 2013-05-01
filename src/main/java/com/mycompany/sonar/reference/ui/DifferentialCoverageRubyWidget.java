20afb0c26c0f75de85ad307394379e333c483c41
unknown <aconfino@gmail.com>
2013-04-30 22:14:02 -0400

    init

---
+++ src/main/java/com/mycompany/sonar/reference/ui/DifferentialCoverageRubyWidget.java
@@ -0,0 +1,30 @@
+package com.mycompany.sonar.reference.ui;
+
+import org.sonar.api.web.AbstractRubyTemplate;
+import org.sonar.api.web.Description;
+import org.sonar.api.web.RubyRailsWidget;
+import org.sonar.api.web.UserRole;
+import org.sonar.api.web.WidgetCategory;
+import org.sonar.api.web.WidgetProperties;
+import org.sonar.api.web.WidgetProperty;
+import org.sonar.api.web.WidgetPropertyType;
+
+@UserRole(UserRole.USER)
+@Description("Shows the differential coverage of a project")
+@WidgetCategory("Differential Coverage")
+
+public class DifferentialCoverageRubyWidget extends AbstractRubyTemplate implements RubyRailsWidget {
+
+  public String getId() {
+    return "differentialCoverage";
+  }
+
+  public String getTitle() {
+    return "Differential Coverage";
+  }
+
+  @Override
+  protected String getTemplatePath() {
+    return "/coverage/differential_coverage.html.erb";
+  }
+}
